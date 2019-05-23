59724de207380941a619848ecb85ffdd7c752d75
KD <extantourlife@gmail.com>
2019-05-23 15:22:59 -0400

    Markettable Controller, still have problems with get request and routing

--- app/controllers/Api/V1/markettables_controller.rb
+++ app/controllers/Api/V1/markettables_controller.rb
@@ -1,11 +1,17 @@
 module Api
-    module v1
-        class MarkettableController < Application
+    module V1
+        class MarkettablesController < Application
             #Action index => listing all of the Markettables
             def index
                 #get all model markettable and order them
                 markettables = Markettable.order('created_at DESC');
                 #19:30
+                render json: {status: 'SUCCESS', message: 'Loaded Data', data: markettables}, status:ok
+            end
+            
+            #show a single one
+            def show
+                markettable = Markettable.find(params[:id])
                 render json: {status: 'SUCCESS', message: 'Loaded Data', data: markettables}, status:ok
             end
         end
